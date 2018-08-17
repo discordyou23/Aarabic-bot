b2b8714a94f3f968028ebbdea70590eaa6cfb8cf
discordyou23 <42048326+discordyou23@users.noreply.github.com>
2018-08-17 15:20:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -151,7 +151,6 @@ client.on('message', msg => {
 
 client.on('message', msg => {
         if (msg.content.startsWith(`=warn`)) {
-          if(!msg.member.hasPermission("MANAGE_MESSAGES")) return;
            let args = msg.content.split(" ").slice(1);
           if (!msg.mentions.members.first()) return msg.reply('منشن الشخص المحدد')
           if (!args[1]) return msg.reply('``اكتب السبب``')
