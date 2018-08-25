9943832f9df4dbc8fb8236d1c13174cdfd505533
Fofodiscord <42048326+discordyou23@users.noreply.github.com>
2018-08-25 04:37:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const convert = require("hh-mm-ss")
+const dateFormat = require('dateformat');
+const fs = require('fs');
 var Canvas = require('canvas')
 var jimp = require('jimp')
 const prefix = "=";
