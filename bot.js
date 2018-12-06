202968e92adb2f9121d2bbf3fe814bc21be5b539
Hacker <42048326+fawazsayed466@users.noreply.github.com>
2018-12-07 00:46:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,7 +86,7 @@ const w = ['./welcome.png'];
                         ctx.fillText(``, 300, 130);
                         
                         ctx.font = "bold 12px Arial";
-                        ctx.fontSize = '20px';
+                        ctx.fontSize = '50px';
                         ctx.fillStyle = "#f1f1f1";
                         ctx.textAlign = "center";
                         ctx.fillText(member.user.username, 200, 150);
